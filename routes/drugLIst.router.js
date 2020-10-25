14aab0087e234f25091a60053d64fa78d2c5df48
SDRoze14 <nratchakomm@gmail.com>
2020-10-26 01:12:28 +0700

    add route

--- routes/drugLIst.router.js
+++ routes/drugLIst.router.js
@@ -3,12 +3,14 @@ const router = express.Router()
 
 const {
   newDrugList,
-  paidDrug
+  paidDrug,
+  deleteDrugList
 } = require('../controller/drugList.controller')
 
 const { isAuthUser, authorizaRoles } = require('../middlewares/auth.middleware')
 
 router.route('/druglist').post(isAuthUser, newDrugList)
 router.route('/druglist/:id').put(isAuthUser, paidDrug)
+router.route('/druglist/:id').put(isAuthUser, paidDrug).delete(deleteDrugList)
 
 module.exports = router
